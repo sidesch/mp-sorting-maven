3dee0e6b0fb40b0cc2ea935a388329727b61388c
sarah_deschamps <deschamps.sias@gmail.com>
2024-11-14 00:19:34 -0600

    Implement Merge Sort and Update README.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Authors
 
 Acknowledgements
 
-* _Forthcoming_.
+* The dnf algorithm in QuickSort was largely based on the algorithm found in this page: https://rebelsky.cs.grinnell.edu/Courses/CSC207/2024Fa/eboards/eboard20-1.html 
 
 This code may be found at <https://github.com/sidesch/mp-sorting-maven>. The original code may be found at <https://github.com/Grinnell-CSC207/mp-sorting-maven>.
 
--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.sorting;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -55,6 +56,63 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    sortHelper(values, 0, values.length);
   } // sort(T[])
+
+  /**
+   * Sorts the values in values from lb (inclusive) to ub (exclusive).
+   * @param values
+   *    The array of values to sort.
+   * @param lb
+   *    The lower bound (inclusive).
+   * @param ub
+   *    The upper bound (exclusive).
+   */
+  private void sortHelper(T[] values, int lb, int ub) {
+    if (ub - lb <= 1) {
+      return;
+    } // if
+    int med = (int)(lb / 2.0 + ub / 2.0);
+    T[] sub1 = Arrays.copyOfRange(values, 0, med);
+    T[] sub2 = Arrays.copyOfRange(values, med, values.length);
+    sortHelper(values, lb, med);
+    sortHelper(values, med, ub);
+    merge(values, lb, med, ub); // the merging is creating problems
+  } // sortHelper(T[], int, int)
+
+  /**
+   * Merges two subarrays.
+   * @param values
+   *    The array of values (containing subarrays).
+   * @param lb
+   *    The lower bound of the first subarray.
+   * @param med
+   *    The upper bound of the first subarray and 
+   *    lower bound of the second subarray.
+   * @param ub
+   *    The upper bound of the second subarray.
+   * @return
+   *    A new sorted merged array.
+   */
+  private void merge(T[] values, int lb, int med, int ub) {
+    int counter = med;
+    T[] copy = values.clone();
+    int idx = lb;
+    while (lb < med && counter < ub) {
+      if (order.compare(copy[lb], copy[counter]) <= 0) {
+        values[idx++] = copy[lb++];
+      } else {
+        values[idx++] = copy[counter++];
+      } // if-else
+    } // while
+    if (lb < med) {
+      for (int i = idx; i < ub; i++) {
+        values[i] = copy[lb++];
+      } // for
+    } else if (counter < ub) {
+      for (int i = idx; i < values.length; i++) {
+        values[i] = copy[counter++];
+      } // for
+    } // if-else
+  } // merge(T[], int, int, int)
 } // class MergeSorter
